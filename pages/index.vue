705a5fef257629fd59d3c47b692a99b295ead7b4
Dimitrio Mandamadiotis <me@dimitriosmandamadiotis.com>
2021-12-15 11:31:36 -0400

    Added tailwindcss v3

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <p>{{ homes[0] }}</p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import homes from '@/data/homes.json'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data () {
+    return {
+      homes
+    }
+  }
+})
 </script>
