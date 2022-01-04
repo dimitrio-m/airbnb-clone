ae5893586b43c081e4c6f40d007312df1bd13e43
Dimitrio Mandamadiotis <me@dimitriosmandamadiotis.com>
2022-01-04 09:48:33 -0400

    Use beforeRouteUpdate instead of watchQuery in search page to evoid known dev bug

--- pages/search.vue
+++ pages/search.vue
@@ -13,6 +13,19 @@ import Home from '@/models/home'
 
 export default Vue.extend({
   name: 'SearchHome',
+  async beforeRouteUpdate (to, _from, next) {
+    const lat = to.query.lat == null || Array.isArray(to.query.lat) ? '' : to.query.lat
+    const lng = to.query.lng == null || Array.isArray(to.query.lng) ? '' : to.query.lng
+    const label = to.query.label == null || Array.isArray(to.query.label) ? '' : to.query.label
+
+    const response = await this.$dataApi.getHomesByLocation(lat, lng)
+    this.homes = response.data as Home[]
+    this.label = label
+    this.lat = lat
+    this.lng = lng
+
+    next()
+  },
   async asyncData (context) {
     const lat = context.query.lat == null || Array.isArray(context.query.lat) ? '' : context.query.lat
     const lng = context.query.lng == null || Array.isArray(context.query.lng) ? '' : context.query.lng
@@ -30,10 +43,9 @@ export default Vue.extend({
     return {
       homes: [] as Home[],
       label: '',
-      lat: 0,
-      lng: 0
+      lat: '',
+      lng: ''
     }
-  },
-  watchQuery: ['lat']
+  }
 })
 </script>
